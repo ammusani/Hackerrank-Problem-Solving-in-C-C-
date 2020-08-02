afa41c8d630f1671f71746709540f71d500fe416
Aliasgar Musani <cs17b004@iittp.ac.in>
2020-08-02 19:08:48 +0530

    Update and rename Solve_Me_First.c to Solve_Me_First.cpp

--- Solve_Me_First.cpp
+++ Solve_Me_First.cpp
@@ -1,27 +1,5 @@
 /*
-Complete the function solveMeFirst to compute the sum of two integers.
-
-Function prototype:
-
-int solveMeFirst(int a, int b);
-
-where,
-
-    a is the first integer input.
-    b is the second integer input
-
-Return values
-
-    sum of the above two integers
-
-Sample Input
-
-a = 2
-b = 3
-
-Sample Output
-
-5
+https://www.hackerrank.com/challenges/solve-me-first/problem
 */
 
 // Solution in C++:
